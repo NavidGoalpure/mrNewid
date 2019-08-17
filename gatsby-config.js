b5421e820b42295fc833d1d167419be2cca72099
Navid Goalpure <Navid.goalpure@gmail.com>
2019-08-17 12:45:17 +0430

    add article about gaverment and crypto

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,6 +51,38 @@ module.exports = {
     `gatsby-plugin-styled-jsx`, // the plugin's code is inserted directly to gatsby-node.js and gatsby-ssr.js files
     `gatsby-plugin-styled-jsx-postcss`, // as above
     {
+      resolve: `gatsby-remark-videos`,
+      options: {
+        pipelines: [
+          {
+            name: "vp9",
+            transcode: chain =>
+              chain
+                .videoCodec("libvpx-vp9")
+                .noAudio()
+                .outputOptions(["-crf 20", "-b:v 0"]),
+            maxHeight: 480,
+            maxWidth: 900,
+            fileExtension: "webm"
+          },
+          {
+            name: "h264",
+            transcode: chain =>
+              chain
+                .videoCodec("libx264")
+                .noAudio()
+                .addOption("-profile:v", "main")
+                .addOption("-pix_fmt", "yuv420p")
+                .outputOptions(["-movflags faststart"])
+                .videoBitrate("1000k"),
+            maxHeight: 480,
+            maxWidth: 900,
+            fileExtension: "mp4"
+          }
+        ]
+      }
+    },
+    {
       resolve: `gatsby-plugin-layout`,
       options: {
         component: require.resolve(`./src/layouts/`)
